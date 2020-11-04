5d3f4c3559abbc8527ed95c361af3ef734324bc5
CAtniz <catniz@hanmail.net>
2020-11-04 22:02:28 +0900

    data set read and display

---
+++ src/chapter2/housing.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+HOUSING_PATH = os.path.join("..", "datasets", "housing")
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+pd.options.display.max_columns = None
+pd.options.display.width = None
+
+housing = load_housing_data()
+print(housing.head())
